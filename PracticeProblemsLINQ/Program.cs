55fff73b794afe6243e1237dcc465716c8ab4d7f
unknown <ana.nalbandyan@gmail.com>
2021-03-12 06:58:24 -0800

    fifth problem

--- PracticeProblemsLINQ/Program.cs
+++ PracticeProblemsLINQ/Program.cs
@@ -51,13 +51,13 @@ namespace PracticeProblemsLINQ
             //Problem 5
             List<string> classGrades = new List<string>()
             {
-                "80,100,92,89,65", 
-                "93,81,78,84,69",
-                "73,88,83,99,64",
-                "98,100,66,74,55"
+                "80,100,92,89,65" 
+                //"93,81,78,84,69",
+                //"73,88,83,99,64",
+                //"98,100,66,74,55" 
             };
 
-            List<string> results4 = LinqProblems.RunProblem1(classGrades);
+            double results4 = LinqProblems.RunProblem5(classGrades);
             Console.WriteLine(results4);
             Console.ReadLine();
 
